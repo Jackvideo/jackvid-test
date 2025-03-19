95b1e5e5812792bc2a99fa7d69bd8765695e1903
jackvideo <13226110808@163.com>
2025-03-19 17:41:12 +0800

    init repo

---
+++ jackvid-test/bk_plugin/forms/1.0.0/form.js
@@ -0,0 +1,12 @@
+(function () {
+    $.atoms["jackvid-test"] = [
+        {
+            "type": "input",
+            "attrs": {
+                "name": "Hello",
+                "hookable": true
+            },
+            "tag_code": "hello"
+        }
+    ]
+})();
